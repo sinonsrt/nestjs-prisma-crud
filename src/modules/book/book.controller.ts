27ac366d548daa581e26a103d0e17427af201692
Lucas Marchiori <sinonsrt@hotmail.com>
2022-05-05 21:49:43 -0300

    create books list and create route

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDTO } from './DTO/book.dto';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() { title, bar_code, description }: BookDTO) {
+    return this.bookService.create({ bar_code, description, title });
+  }
+
+  @Get()
+  async listAll() {
+    return this.bookService.findAll();
+  }
 }
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/PrismaService';
+import { BookDTO } from './DTO/book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+
+  async create({ title, bar_code, description }: BookDTO) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code,
+      },
+    });
+
+    if (bookExists) {
+      throw new Error('Book already exists!');
+    }
+
+    const book = await this.prisma.book.create({
+      data: {
+        title,
+        bar_code,
+        description,
+      },
+    });
+
+    return book;
+  }
+
+  async findAll() {
+    return this.prisma.book.findMany();
+  }
+}
